762a79c138c7836b8793ff92d2773b5db783edce
Joe Brew <joebrew@gmail.com>
2019-12-09 08:03:04 +0200

    updated

---
+++ scripts/recon_excel_to_xml.py
@@ -0,0 +1,32 @@
+import pygsheets
+import xlrd
+import os
+#import numpy as np
+
+# Authorize by using this: https://pygsheets.readthedocs.io/en/latest/authorization.html
+gc = pygsheets.authorize('../credentials/gsheets_oauth.json')
+
+# Define the documents
+# docs = ['censushouse', 'censusmember']
+#for doc in docs:
+    # Open spreadsheet and then workseet
+#    sh = gc.open(doc)
+#    wks = sh.sheet1
+
+    #export as xls
+#    wks.export(pygsheets.ExportType.XLS, doc)
+
+    ## Convert to xml
+#    os.system('xls2xform ' + doc + '.xls ' + doc +'.xml')
+
+doc = 'bohemiarecon'
+sh = gc.open(doc)
+wks = sh.sheet1
+
+#export as xls
+wks.export(pygsheets.ExportType.XLS, doc)
+
+## Convert to xml
+os.system('xls2xform ' + doc + '.xls ' + doc +'.xml')
+
+print("Successfully created the following documents:\n---" + doc + '.xls\n---' + doc +'.xml')
