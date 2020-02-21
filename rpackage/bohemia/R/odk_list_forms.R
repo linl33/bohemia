16bd50ebb285707a0b1cbe52937ac7944adda576
Joe Brew <joebrew@gmail.com>
2020-02-21 12:08:25 +0300

    list forms improvement

--- rpackage/bohemia/R/odk_list_forms.R
+++ rpackage/bohemia/R/odk_list_forms.R
@@ -2,19 +2,36 @@
 #' 
 #' List the forms on an ODK server
 #' @param url The URL of the ODK Aggregate server, default being https://bohemia.systems, without a trailing dash
+#' @param user The ODK Aggregate username
+#' @param password The ODK Aggregate password
 #' @import httr
 #' @import xml2
 #' @import dplyr
 #' @return A tibble with columns name, id, and url
 #' @export
 
-odk_list_forms <- function(url = 'https://bohemia.systems'){
+odk_list_forms <- function(url = 'https://bohemia.systems',
+                           user = NULL,
+                           password = NULL){
+  
+  # Ensure that username and password are provided
+  if(is.null(user) | is.null(password)){
+    message('No user/password were entered. Will try with it. If the server requires it, you\'ll get a 401 error')
+  }
   
   # Create the url for the forms list
   fl_url <- paste0(url, '/formList')
   
   # Carry out the GET request
-  r <- GET(fl_url)
+  if(is.null(user)){
+    r <- GET(fl_url)
+  } else {
+    r <- GET(fl_url,
+             authenticate(user = user,
+                          password = password, 
+                          type = 'digest'))
+  }
+  
   
   # Provide info on the request (and stop if error)
   stop_for_status(r) 
