c7e07f5475f7396baf5a1cd7964fe36a201499be
benmbrew <benmbrew@gmail.com>
2020-08-27 21:25:09 -0400

    creadted very bare minimum app

---
+++ rpackage/bohemia/inst/shiny/minicensus/app.R
@@ -0,0 +1,104 @@
+library(shiny)
+library(shinydashboard)
+if('temp_data.RData' %in% dir()){
+    load('temp_data.RData')
+} else {
+    source('global.R')
+}
+# how are credentials used, where should i store them
+# am i adding to the credentials file
+
+# use function to get odk data using minicensus as name 
+header <- dashboardHeader(title = tags$a(tags$img(src='logo.png',height='32',width='36', alt = 'DataBrew')))
+sidebar <- dashboardSidebar(
+    sidebarMenu(
+        menuItem(
+            text="Main",
+            tabName="main",
+            icon=icon("archway")),
+        menuItem(
+            text = 'About',
+            tabName = 'about',
+            icon = icon("cog", lib = "glyphicon"))
+    )
+)
+
+body <- dashboardBody(
+    tags$head(
+        tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")
+    ),
+    tabItems(
+        tabItem(
+            tabName="main",
+            fluidPage(
+                fluidRow(
+                    column(4,
+                           selectInput(inputId = 'fw_id',
+                                       label = 'Select FW',
+                                       choices = wid_names, 
+                                       selected = wid_names,
+                                       multiple = TRUE),
+                           selectInput(inputId = 'q_id',
+                                       label = 'Select Question',
+                                       choices = sort(unique(temp$question)), 
+                                       selected = wid_names,
+                                       multiple = TRUE)),
+                    column(8,
+                           plotOutput('fw_plot'),
+                           DT::dataTableOutput('fw_table'))
+                )
+                
+            )
+        ),
+        tabItem(
+            tabName = 'about',
+            fluidPage(
+                fluidRow(
+                    div(img(src='logo.png', align = "center"), style="text-align: center;"),
+                    h4('Built in partnership with ',
+                       a(href = 'http://databrew.cc',
+                         target='_blank', 'Databrew'),
+                       align = 'center'),
+                    p('Empowering research and analysis through collaborative data science.', align = 'center'),
+                    div(a(actionButton(inputId = "email", label = "info@databrew.cc", 
+                                       icon = icon("envelope", lib = "font-awesome")),
+                          href="mailto:info@databrew.cc",
+                          align = 'center')), 
+                    style = 'text-align:center;'
+                )
+            )
+        )
+    )
+)
+
+# UI
+ui <- dashboardPage(header, sidebar, body, skin="blue", title = 'databrew')
+
+# Server
+server <- function(input, output, session) {
+    
+    
+    output$fw_table <- DT::renderDataTable({
+        # get inputs
+        
+        fw_id <- input$fw_id
+        q_id <- input$q_id
+        if(length(fw_id) == 1){
+            if(fw_id == '333'){
+                temp <- temp %>% select(question, key, test_333, wrong_333)
+            } else {
+                temp <- temp %>% select(question, key, test_444, wrong_444)
+            }
+        }
+        
+        
+        DT::datatable(temp)
+    })
+    
+    output$fw_plot <- renderPlot({
+        ggplot()
+    })
+    
+    
+}
+shinyApp(ui, server)#
---
+++ rpackage/bohemia/inst/shiny/minicensus/global.R
@@ -0,0 +1,70 @@
+
+# get data from server
+library(bohemia)
+library(yaml)
+library(reshape2)
+library(tidyverse)
+
+# read in creds
+creds <- read_yaml('../../../../../credentials/credentials.yaml')
+
+# GET TEST DATA
+data <- odk_get_data(
+  url = creds$databrew_odk_server,
+  id = 'minicensus',
+  id2 = NULL,
+  unknown_id2 = TRUE,
+  uuids = NULL,
+  exclude_uuids = NULL,
+  user = creds$databrew_odk_user,
+  password = creds$databrew_odk_pass
+)
+
+# ADD IN FAKE IDS
+temp <- data[[2]]
+temp$wid_manual <- c(111,222,333,444)
+temp <- temp[,colSums(is.na(temp))<nrow(temp)]
+# temp <- melt(temp, id.vars = 'wid_manual')
+temp <- temp[-2,]
+temp_key <- temp[1,]
+temp <- temp[2:3,]
+
+# compare data
+wid_names <- sort(unique(temp$wid_manual))
+i=1
+dat_list <- list()
+for(i in 1:length(unique(wid_names))){
+  this_name <- wid_names[i]
+  sub_form <- temp %>% filter(wid_manual == this_name)
+  test_form <- rbind(sub_form,temp_key)
+  test_form<-as.data.frame(t(test_form))
+  names(test_form) <- c('test', 'key')
+  test_form$question <- rownames(test_form)
+  test_form$test <- as.character(test_form$test)
+  test_form$key <- as.character(test_form$key)
+  
+  test_form$wrong <- test_form$test != test_form$key
+  test_form$wrong[is.na(test_form$wrong)] <- TRUE
+  dat_list[[i]] <- test_form
+  
+}
+
+temp1 <- dat_list[[1]]
+names(temp1)[1] <- paste0('test_', wid_names[1])
+names(temp1)[4] <- paste0('wrong_', wid_names[1])
+names(temp2)[1] <- paste0('test_', wid_names[2])
+names(temp2)[4] <- paste0('wrong_', wid_names[2])
+
+temp <- inner_join(temp1, temp2)
+temp <- temp[, c('question', 'key', 'test_333', 'wrong_333', 'test_444', 'wrong_444')]
+save.image('temp_data.RData')
+rm(creds,dat_list, data, sub_form, temp_key, test_form)
+# temp <- melt(temp, id.vars = 'question')
+
+# PLACEHOLDER FOR GETTING FORM DATA
+
+# PLACEHOLDER FOR GETTING ANSWER KEY DATA
+
+
+
+
