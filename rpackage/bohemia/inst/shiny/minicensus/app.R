92023ab3d35f9d0f61f6909a07f3134aba3591b8
Joe Brew <joebrew@gmail.com>
2020-08-30 01:00:51 +0200

    minor

--- rpackage/bohemia/inst/shiny/minicensus/app.R
+++ rpackage/bohemia/inst/shiny/minicensus/app.R
@@ -11,12 +11,16 @@ if('temp_data.RData' %in% dir()){
 # am i adding to the credentials file
 
 # use function to get odk data using minicensus as name 
-header <- dashboardHeader(title = tags$a(tags$img(src='logo.png',height='32',width='36', alt = 'DataBrew')))
+header <- dashboardHeader(title = 'Minicensus grader')
 sidebar <- dashboardSidebar(
     sidebarMenu(
         menuItem(
             text="Main",
             tabName="main",
+            icon=icon("home")),
+        menuItem(
+            text="Details",
+            tabName="details",
             icon=icon("archway")),
         menuItem(
             text = 'About',
@@ -33,30 +37,37 @@ body <- dashboardBody(
         tabItem(
             tabName="main",
             fluidPage(
+                DT::dataTableOutput('agg_table')
+            )),
+        tabItem(
+            tabName = 'details',
+            fluidPage(
                 fluidRow(
-                    column(4,
+                    column(12,
+                           h3('Fieldworkers'),
                            selectInput(inputId = 'fw_id',
-                                       label = 'Select FW',
-                                       choices = c('fw1', 'fw2', 'fw3'), 
-                                       selected = c('fw1', 'fw2', 'fw3'),
-                                       multiple = TRUE),
+                                       label = 'Select FW ID',
+                                       choices = agg$`Worker ID`, 
+                                       multiple = FALSE),
+                           DT::dataTableOutput('fw_table'),
+                           h3('Questions'),
                            selectInput(inputId = 'q_id',
                                        label = 'Select Question',
-                                       choices = sort(unique(temp$question)), 
-                                       selected = sort(unique(temp$question)), 
-                                       multiple = TRUE)),
-                    column(8,
-                           plotOutput('fw_plot'),
-                           DT::dataTableOutput('fw_table'))
+                                       choices = sort(unique(final$Variable))),
+                           DT::dataTableOutput('q_table'),
+                           h3('Complete data'),
+                           DT::dataTableOutput('overall_table'))
                 )
                 
             )
+            
+            
         ),
         tabItem(
             tabName = 'about',
             fluidPage(
                 fluidRow(
-                    div(img(src='logo.png', align = "center"), style="text-align: center;"),
+                    # div(img(src='logo.png', align = "center"), style="text-align: center;"),
                     h4('Built in partnership with ',
                        a(href = 'http://databrew.cc',
                          target='_blank', 'Databrew'),
@@ -81,44 +92,32 @@ server <- function(input, output, session) {
     
     
     output$fw_table <- DT::renderDataTable({
-        # get inputs
-        fw_id <- c('fw1', 'fw2', 'fw3')
-        q_id <- sort(unique(temp$question))
-        fw_id <- input$fw_id
-        q_id <- input$q_id
-        
-        temp <- temp %>% 
-            select(question, key, fw_id) %>% 
-            filter(question %in% q_id)
-        
-        DT::datatable(temp)
+        fid <- input$fw_id
+        if(is.null(fid)){
+            out <- NULL
+        } else{
+            out <- done %>% filter(`Worker ID` == fid)
+        }
+        return(out)
     })
     
-    output$fw_plot <- renderPlot({
-        # get inputs
-        fw_id <- c('fw1', 'fw2', 'fw3')
-        q_id <- sort(unique(temp$question))
-        fw_id <- input$fw_id
-        q_id <- input$q_id
-        
-        temp <- temp %>% 
-            select(question, key, fw_id) %>% 
-            filter(question %in% q_id)
-        
-        temp_key <- temp %>% select(question, key)
-        temp_test <- temp %>% select(question, fw_id)
-        
-        temp_test <- melt(temp_test, id.vars = c('question'))
-        temp_test <- left_join(temp_test, temp_key)
-        temp_test$answer <- ifelse(temp_test$value != temp_test$key, 'wrong', 'right')
-        temp_test$answer[is.na(temp_test$answer)] <- 'wrong'
-
-        temp_test <- temp_test %>% group_by(variable) %>% summarise(sum_right = sum(answer == 'right'),
-                                                                     sum_wrong = sum(answer == 'wrong'))
-        temp_test$percent_correct = round(temp_test$sum_right/(temp_test$sum_right+temp_test$sum_wrong)*100,2)
-        ggplot(temp_test, aes(variable, percent_correct)) + geom_bar(stat='identity')
+    output$q_table <- DT::renderDataTable({
+        fid <- input$q_id
+        if(is.null(fid)){
+            out <- NULL
+        } else{
+            out <- done %>% filter(Variable == fid)
+        }
+        return(out)
     })
     
+    output$overall_table <- DT::renderDataTable({
+        done
+    })
+    
+    output$agg_table <- DT::renderDataTable({
+        agg
+    })
     
 }
 shinyApp(ui, server)#
--- rpackage/bohemia/inst/shiny/minicensus/global.R
+++ rpackage/bohemia/inst/shiny/minicensus/global.R
@@ -4,85 +4,162 @@ library(bohemia)
 library(yaml)
 library(reshape2)
 library(tidyverse)
+library(readxl)
 
-# read in creds
-creds <- read_yaml('../../../../../credentials/credentials.yaml')
-
-# GET TEST DATA
-data <- odk_get_data(
-  url = creds$databrew_odk_server,
-  id = 'minicensus',
-  id2 = NULL,
-  unknown_id2 = FALSE,
-  uuids = NULL,
-  exclude_uuids = NULL,
-  user = creds$databrew_odk_user,
-  password = creds$databrew_odk_pass
-)
-
-# ADD IN FAKE IDS
-temp <- data[[2]]
-temp$wid_manual <- c(111,222,333,444)
-temp <- temp[,colSums(is.na(temp))<nrow(temp)]
-
-# HERE MAKE SHORT LIST OF QUESTIONS TO MAKE SIMPLER
-questions_list <- names(temp)[grepl('hh_|wid_manual', names(temp))]
-
-# SUBSET DATA BY QUESTIONS 
-temp <- temp[, questions_list]
-temp <- as.data.frame(t(temp))
-
-# ARBITRAILITY ASSIGN ONE COLUMNS AS KEY AND THE OTHER FAKE FW_IDS
-names(temp) <- c('key', 'fw1', 'fw2', 'fw3')
-temp$question <- rownames(temp)
-rownames(temp) <- NULL
-temp <- temp[, c('question', 'key', 'fw1', 'fw2', 'fw3')]
-save.image('temp_data.RData')
-
-
-# 
-# 
-# # temp <- melt(temp, id.vars = 'wid_manual')
-# temp <- temp[-2,]
-# temp_key <- temp[1,]
-# temp <- temp[2:3,]
-# 
-# # compare data
-# wid_names <- sort(unique(temp$wid_manual))
-# i=1
-# dat_list <- list()
-# for(i in 1:length(unique(wid_names))){
-#   this_name <- wid_names[i]
-#   sub_form <- temp %>% filter(wid_manual == this_name)
-#   test_form <- rbind(sub_form,temp_key)
-#   test_form<-as.data.frame(t(test_form))
-#   names(test_form) <- c('test', 'key')
-#   test_form$question <- rownames(test_form)
-#   test_form$test <- as.character(test_form$test)
-#   test_form$key <- as.character(test_form$key)
-#   
-#   test_form$wrong <- test_form$test != test_form$key
-#   test_form$wrong[is.na(test_form$wrong)] <- TRUE
-#   dat_list[[i]] <- test_form
-#   
-# }
-# 
-# temp1 <- dat_list[[1]]
-# names(temp1)[1] <- paste0('test_', wid_names[1])
-# names(temp1)[4] <- paste0('wrong_', wid_names[1])
-# names(temp2)[1] <- paste0('test_', wid_names[2])
-# names(temp2)[4] <- paste0('wrong_', wid_names[2])
-# 
-# temp <- inner_join(temp1, temp2)
-# temp <- temp[, c('question', 'key', 'test_333', 'wrong_333', 'test_444', 'wrong_444')]
-# save.image('temp_data.RData')
-# rm(creds,dat_list, data, sub_form, temp_key, test_form)
-# # temp <- melt(temp, id.vars = 'question')
-# 
-# # PLACEHOLDER FOR GETTING FORM DATA
-# 
-# # PLACEHOLDER FOR GETTING ANSWER KEY DATA
-# 
-# 
-# 
-# 
+if('data.RData' %in% dir()){
+  load('data.RData')
+} else {
+  
+  # Read in xls form
+  xf <- read_excel('minicensus.xlsx') %>% dplyr::select(name,
+                                                        en = `label::English`,
+                                                        sw = `label::Swahili`)
+  
+  # read in creds
+  creds <- read_yaml('credentials.yaml')
+  
+  # Read in answer key
+  data <- odk_get_data(
+    url = creds$tza_odk_server,
+    id = 'minicensus',
+    id2 = NULL,
+    unknown_id2 = FALSE,
+    # uuids = NULL,
+    exclude_uuids = NULL,
+    # uuids = c("uuid:06e9b414-4c82-4097-bd4a-e31065f1265e",
+    #           "uuid:641eaadd-1e47-4e48-99c2-02c13de93b0a",
+    #           "uuid:bab37889-d8a8-4b6c-984c-a99e2b9926ce"),
+    user = creds$tza_odk_user,
+    password = creds$tza_odk_pass
+  )
+  
+  # Get answer keys
+  answer_uuids <- c("uuid:06e9b414-4c82-4097-bd4a-e31065f1265e",
+                    "uuid:641eaadd-1e47-4e48-99c2-02c13de93b0a",
+                    "uuid:bab37889-d8a8-4b6c-984c-a99e2b9926ce")
+  
+  # Divide answers vs non-answers
+  repeat_names <- names(data$repeats)
+  non_answer_repeats <- answer_repeats <- list()
+  for(i in 1:length(repeat_names)){
+    this_repeat_name <- repeat_names[i]
+    this_repeat_data <- data$repeats[[this_repeat_name]]
+    this_answer_repeats <- this_repeat_data %>% filter(instanceID %in% answer_uuids)
+    this_non_answer_repeats <- this_repeat_data %>% filter(!instanceID %in% answer_uuids)
+    answer_repeats[[i]] <- this_answer_repeats
+    non_answer_repeats[[i]] <- this_non_answer_repeats
+  }
+  fw_repeats <- non_answer_repeats
+  names(answer_repeats) <-  names(fw_repeats) <- repeat_names
+  
+  # Compare non-answers with answers
+  fw_non_repeats <- data$non_repeats %>% filter(!instanceID %in% answer_uuids)
+  answer_non_repeats <- data$non_repeats %>% filter(instanceID %in% answer_uuids)
+  
+  # # We now have 4 objects
+  # fw_repeats
+  # fw_non_repeats
+  # answer_repeats
+  # answer_non_repeats
+  
+  # Define those vars we never want to grade
+  dont_evaluate <- c('instanceID',
+                     'device_id',
+                     'end_time',
+                     'hh_photograph',
+                     'instanceName',
+                     'start_time',
+                     'todays_date',
+                     'wid',
+                     'wid_manual',
+                     'repeat_name',
+                     'repeated_id')
+  # Grade
+  out_list <- list()
+  wrong_hamlet_list <- c()
+  for(i in 1:nrow(fw_non_repeats)){
+    message('i is ', i)
+    no_answer <- FALSE
+    # Get the non repeats
+    this_row <- fw_non_repeats[i,]
+    wid <- this_row$wid
+    the_uuid <- this_row$instanceID
+    this_hamlet_id <- this_row$hh_hamlet_code
+    this_answer <- answer_non_repeats %>% filter(hh_hamlet_code == this_hamlet_id)
+    if(nrow(this_answer) < 1){
+      wrong_hamlet_list <- c(wrong_hamlet_list, wid)
+    } else {
+      answer_uuid <- this_answer$instanceID
+      answer_nas <- as.logical(is.na(this_answer)) | as.logical(this_answer == '()')
+      this_answer <- this_answer[,!answer_nas]
+      this_row <- this_row[,!answer_nas]
+      this_answer <- this_answer[,!names(this_answer) %in% dont_evaluate]
+      this_row <- this_row[,!names(this_row) %in% dont_evaluate]
+      response_vector <- as.logical(this_row == this_answer)
+      label_df <- tibble(name = names(this_row)) %>% left_join(xf)
+      out <- tibble(`Worker ID` = wid,
+                    `Variable` = names(this_row),
+                    `Label (en)` = label_df$en,
+                    `Label (sw)` = label_df$sw,
+                    `Supplied answer` = as.character(this_row),
+                    `Correct answer` = as.character(this_answer),
+                    `Status` = ifelse(response_vector, 'Correct', 'Incorrect'))
+      
+      # Go through each repeat and get those correct answers too
+      rep_list <- list()
+      counter <- 0
+      for(r in 1:length(repeat_names)){
+        print(r)
+        this_repeat_name <- repeat_names[r]
+        this_repeat_data <- fw_repeats[[this_repeat_name]]
+        this_row <- this_repeat_data %>% filter(instanceID == the_uuid)
+        this_answer <- answer_repeats[[this_repeat_name]] %>% filter(instanceID == answer_uuid)
+        # answer_nas <- as.logical(is.na(this_answer)) | as.logical(this_answer == '()')
+        # this_answer <- this_answer[,!answer_nas]
+        # this_row <- this_row[,!answer_nas]
+        this_answer <- this_answer[,!names(this_answer) %in% dont_evaluate]
+        this_row <- this_row[,!names(this_row) %in% dont_evaluate]
+        for(x in 1:nrow(this_answer)){
+          x_answer <- this_answer[x,]
+          x_row <- this_row[x,]
+          response_vector <- as.logical(x_row == x_answer)
+          
+          label_df <- tibble(name = names(x_answer)) %>% left_join(xf)
+          outx <- tibble(`Worker ID` = wid,
+                         `Variable` = names(x_answer),
+                         `Label (en)` = label_df$en,
+                         `Label (sw)` = label_df$sw,
+                         `Supplied answer` = as.character(this_row),
+                         `Correct answer` = as.character(this_answer),
+                         `Status` = ifelse(response_vector, 'Correct', 'Incorrect'))
+          counter <- counter + 1
+          rep_list[[counter]] <- outx
+        }
+      }
+      reps <- bind_rows(rep_list)
+      final <- bind_rows(
+        out,
+        reps
+      )
+      out_list[[i]] <- final
+    }
+  }
+  
+  done <- bind_rows(out_list)
+  wrong_hamlet_list
+  
+  # Clean up a bit
+  done <- done %>% filter(!grepl('note_', Variable)) %>%
+    filter(!grepl('_count', Variable)) %>%
+    filter(!is.na(`Correct answer`)) %>%
+    filter(`Correct answer` != 'NA')
+  
+  # Get aggregated
+  agg <- done %>%
+    group_by(`Worker ID`) %>%
+    summarise(Correct = length(which(Status == 'Correct')),
+              Incorrect = length(which(Status == 'Incorrect')),
+              Percent = round(Correct / (Correct+Incorrect) * 100, digits = 2))
+  save(agg, done, wrong_hamlet_list, xf, final,
+       file = 'data.RData')
+}
