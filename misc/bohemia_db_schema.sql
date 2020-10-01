e846f115109617ca768c098f0144622924125321
Joe Brew <joebrew@gmail.com>
2020-10-01 08:57:51 +0200

    combo

--- misc/bohemia_db_schema.sql
+++ misc/bohemia_db_schema.sql
@@ -7,11 +7,90 @@ CREATE DATABASE bohemia;
 CREATE USER bohemia_app WITH LOGIN PASSWORD 'riscrazy';
 -- NOTE switch to the bohemia database before proceeding with the following queries execution.
 -- psql command `\c bohemia`
+-- on ubuntu: export PGPASSWORD='riscrazy'; psql -h 'localhost' -U 'bohemia_app' -d 'bohemia'
 
 CREATE TABLE minicensus_main (
-    instance_id  uuid,
-    household_data  jsonb NOT NULL,
+    instance_id   uuid, 
+    any_deaths_past_year   VARCHAR(16),
+    cook_main_water_source   VARCHAR(64),
+    cook_time_to_water   VARCHAR(64),
+    device_id   VARCHAR(64),
+    end_time   TIMESTAMP,
+    have_wid   VARCHAR(64),
+    hh_animals_distance_cattle_dry_season   VARCHAR(64),
+    hh_animals_distance_cattle_dry_season_geo   VARCHAR(256),
+    hh_animals_distance_cattle_rainy_season   VARCHAR(64),
+    hh_animals_distance_cattle_rainy_season_geo   VARCHAR(256),
+    hh_animals_distance_cattle_sleep   VARCHAR(64),
+    hh_animals_dry_season_distance_pigs   VARCHAR(64),
+    hh_animals_dry_season_geo_pigs   VARCHAR(256),
+    hh_animals_dry_season_pigs   VARCHAR(64),
+    hh_animals_rainy_season_distance_pigs   VARCHAR(64),
+    hh_animals_rainy_season_geo_pigs   VARCHAR(256),
+    hh_animals_rainy_season_pigs   VARCHAR(64),
+    hh_animals_where_cattle_dry_season   VARCHAR(64),
+    hh_animals_where_cattle_rainy_season   VARCHAR(64),
+    hh_contact_info_number   VARCHAR(64),
+    hh_contact_info_number_alternate   VARCHAR(64),
+    hh_contact_info_number_can_call   VARCHAR(64),
+    hh_country   VARCHAR(64),
+    hh_district   VARCHAR(64),
+    hh_geo_location   VARCHAR(256),
+    hh_hamlet   VARCHAR(64),
+    hh_hamlet_code   VARCHAR(3),
+    hh_have_paint_house   VARCHAR(64),
+    hh_have_paint_worker   VARCHAR(64),
+    hh_head_dob   DATE,
+    hh_head_gender   VARCHAR(8),
+    hh_head_id   VARCHAR(11),
+    hh_head_months_away   INT,
+    hh_health_other   VARCHAR(64),
+    hh_health_permission   VARCHAR(128),
+    hh_health_who   VARCHAR(64),
+    hh_health_who_other   VARCHAR(64),
+    hh_id   VARCHAR(7),
+    hh_main_building_type   VARCHAR(64),
+    hh_main_energy_source_for_lighting   VARCHAR(64),
+    hh_main_wall_material   VARCHAR(128),
+    hh_member_num   INT,
+    hh_member_num_non_residents   INT,
+    hh_member_num_residents   INT,
+    hh_n_constructions   INT,
+    hh_n_constructions_sleep   INT,
+    hh_n_cows_greater_than_1_year   INT,
+    hh_n_cows_less_than_1_year   INT,
+    hh_n_pigs_greater_than_6_weeks   INT,
+    hh_n_pigs_less_than_6_weeks   INT,
+    hh_owns_cattle_or_pigs   VARCHAR(16),
+    hh_photograph   VARCHAR(64),
+    hh_possessions   VARCHAR(128),
+    hh_region   VARCHAR(64),
+    hh_size   INT,
+    hh_village   VARCHAR(64),
+    hh_ward   VARCHAR(64),
+    how_many_deaths   INT,
+    instanceName   VARCHAR(256),
+    irs_past_12_months   VARCHAR(256),
+    n_nets_in_hh   INT,
+    respondent_id   VARCHAR(256),
+    start_time   TIMESTAMP,
+    todays_date   DATE,
+    water_bodies   VARCHAR(256), 
+    water_bodies_how_many   INT,
+    wid   INT,
+    wid_manual   VARCHAR(32),
+    wid_qr   VARCHAR(32),
     PRIMARY KEY(instance_id)
+);
+
+CREATE TABLE minicensus_people (
+    instance_id    uuid,
+    first_name   VARCHAR(64),
+    last_name   VARCHAR(64),
+    pid   VARCHAR(11),
+    name_label   VARCHAR(128),
+    num  INT,
+    CONSTRAINT fk_minicensus_main FOREIGN KEY (instance_id) REFERENCES minicensus_main(instance_id) ON DELETE CASCADE
 );
 
 CREATE TABLE minicensus_repeat_death_info (
@@ -23,10 +102,10 @@ CREATE TABLE minicensus_repeat_death_info (
     death_age     INT,                                           
     death_age_unit    VARCHAR(8),
     death_dob     DATE, 
-    death_dob_known   BOOLEAN, 
+    death_dob_known   VARCHAR(16), 
     death_dob_unknown     INT,
     death_dod     DATE, 
-    death_dod_known   BOOLEAN, 
+    death_dod_known   VARCHAR(16), 
     death_gender  VARCHAR(8), 
     death_id  TEXT,
     death_location    VARCHAR(128), 
@@ -38,7 +117,7 @@ CREATE TABLE minicensus_repeat_death_info (
     non_default_death_id  TEXT, 
     note_death_id     TEXT,
     repeat_death_info_count   INT, 
-    trigger_non_default_death_id  BOOLEAN,
+    trigger_non_default_death_id   VARCHAR(16),
     CONSTRAINT fk_minicensus_main FOREIGN KEY (instance_id) REFERENCES minicensus_main(instance_id) ON DELETE CASCADE
 );
 
@@ -51,8 +130,7 @@ CREATE TABLE minicensus_repeat_hh_sub (
     hh_sub_gender     VARCHAR(8),
     hh_sub_id     INT,
     hh_sub_relationship   VARCHAR(64),
-    hh_sub_relationship_other     VARCHAR(64),
-    note_hh_head_is_sub   BOOLEAN,
+    hh_sub_relationship_other     VARCHAR(64)
     CONSTRAINT fk_minicensus_main FOREIGN KEY (instance_id) REFERENCES minicensus_main(instance_id) ON DELETE CASCADE
 );
 
@@ -63,9 +141,8 @@ CREATE TABLE minicensus_repeat_household_members_enumeration (
     dob   DATE,
     first_name    VARCHAR(256),
     gender    VARCHAR(8),
-    hh_member_adjus  INT, -- TODO fix label
+    hh_member_adjustment  INT, 
     hh_member_number  INT,
-    hh_member_number_size     VARCHAR(8), 
     last_name     VARCHAR(256), -- the first name had multiple names stored, applying the same here 
     member_resident   VARCHAR(8),
     non_default_id    TEXT, 
@@ -81,7 +158,7 @@ CREATE TABLE minicensus_repeat_mosquito_net (
 --   repeated_id <dbl> - # used to associate data for a single row
     instance_id    uuid,     
     net_obtain_when   DATE,
-    repeat_mosquito   INT, -- TODO fix label
+    num   INT, 
     CONSTRAINT fk_minicensus_main FOREIGN KEY (instance_id) REFERENCES minicensus_main(instance_id) ON DELETE CASCADE
 );
 
@@ -89,7 +166,7 @@ CREATE TABLE minicensus_repeat_water (
 --   repeat_name <chr> - # table name 
 --   repeated_id <dbl> - # used to associate data for a single row 
     instance_id    uuid,
-    repeat_water_co  INT, -- TODO fix label
-    water_bodies_ty  VARCHAR(64), -- TODO fix label
+    num  INT, 
+    water_bodies_type  VARCHAR(64), 
     CONSTRAINT fk_minicensus_main FOREIGN KEY (instance_id) REFERENCES minicensus_main(instance_id) on delete CASCADE
 );                                    
--- misc/kate_example.R
+++ misc/kate_example.R
@@ -13,6 +13,7 @@ con <- dbConnect(drv, dbname='bohemia', host='localhost', port=5432, user='bohem
 dbExistsTable(con, 'minicensus_main')
 
 existing_uuids <- dbGetQuery(con, 'SELECT instance_id FROM minicensus_main')
+if(nrow(existing_uuids)< 0){ existing_uuids <- c()} else {existing_uuids <- existing_uuids$instance_id} 
 
 data <- odk_get_data(
   url = url,
@@ -30,42 +31,128 @@ data <- odk_get_data(
 # 1. non_repeats: this is the main dataset. we should call it minicensus_main or something similar in psql
 # 2. repeats: these are the nested datasets. each has its own name. each should get its own table in psql
 
-# Save the data to the DB
-for (i in 1:nrow(data['non_repeats'][[1]])){
-  instance_id <- data['non_repeats'][[1]]['instanceID'][[1]][[1]]
-  # household_data <- data['non_repeats'][[1]][['rest_of_the_list']]
-  insert_data = data.frame(instance_id = instance_id, household_data = household_data)
+# Write main table (non repeats)
+library(dplyr)
+format_minicensus <- function(data){
+  
+  ## MAIN PART OF MINICENSUS
+  df <- data$non_repeats
+  df <- df %>% dplyr::rename(instance_id = instanceID)
+  # Extract the people columns
+  people_columns <- c(paste0('first_name', 1:30),
+                      paste0('last_name', 1:30),
+                      paste0('pid', 1:30),
+                      paste0('name_label', 1:30))
+  # Divide between the people part and non people part
+  people_part <- df[,people_columns] %>% mutate(instance_id = df$instance_id)
+  df <- df[,!names(df) %in% people_columns]
+  # Remove all the notes
+  df <- df[,!grepl('note_', names(df))]
+  df <- df[,!names(df) %in% c('animal_house_distance_pigs', 
+                              'hh_animals_animal_house_location_pigs',
+                              'hh_member_note',
+                              'hh_no_paint_problem',
+                              'hh_owns_cattle_or_pigs_note',
+                              'hh_paint_note',
+                              'hh_region_show',
+                              'other_decider')]
+  # Clean up the people part
+  people_list <- list()
+  counter <- 0
+  for(i in 1:nrow(people_part)){
+    for(j in 1:30){
+      counter <- counter + 1
+      these_columns <- c(paste0('first_name', j),
+                         paste0('last_name', j),
+                         paste0('pid', j),
+                         paste0('name_label', j))
+      these_data <- people_part[i,these_columns]
+      names(these_data) <- c('first_name', 'last_name', 'pid', 'name_label')
+      these_data$num <- j
+      these_data$instance_id <- people_part$instance_id[i]
+      people_list[[counter]] <- these_data
+    }
+  }
+  people_part <- bind_rows(people_list)
+  people_part <- people_part %>% dplyr::filter(!is.na(pid))
+  # Clean up some variables
+  df <- df %>%
+    # Hamlet and village clean-up
+    mutate(hh_hamlet = ifelse(!is.na(hh_hamlet_other),
+                              hh_hamlet_other,
+                              hh_hamlet)) %>%
+    mutate(hh_village = ifelse(!is.na(hh_village_other),
+                               hh_village_other,
+                              hh_village)) %>%
+    dplyr::select(-hh_hamlet_code_list,
+                  -hh_hamlet_code_not_list,
+                  -hh_hamlet_other,
+                  -hh_village_other,
+                  -hh_other_location) %>%
+    # Household ID clean up
+    dplyr::select(-hh_id_manual) %>% # we can remove because hh_id just copies it
+    # Main building type clean up
+    mutate(hh_main_building_type = ifelse(!is.na(hh_main_building_type_other),
+                                          hh_main_building_type_other,
+                                          hh_main_building_type)) %>%
+    dplyr::select(-hh_main_building_type_other) %>%
+    # lighting clean up
+    mutate(hh_main_energy_source_for_lighting = ifelse(!is.na(hh_main_energy_source_for_lighting_other),
+                                                              hh_main_energy_source_for_lighting_other,
+                                                              hh_main_energy_source_for_lighting)) %>%
+    # wall material clean up
+    mutate(hh_main_wall_material = ifelse(!is.na(hh_main_wall_material_free),
+                                                 hh_main_wall_material_free,
+                                                 hh_main_wall_material)) %>%
+    mutate(hh_health_permission = ifelse(!is.na(hh_health_permission_other),
+                                         hh_health_permission_other,
+                                         hh_health_permission))
+  
+  # REPEATS
+  # death
+  repeat_death_info <- data$repeats$repeat_death_info %>%
+    dplyr::rename(instance_id = instanceID) %>% dplyr::select(-repeat_name, -repeated_id)
+  # hh_sub
+  repeat_hh_sub <- data$repeats$repeat_hh_sub %>%
+    dplyr::rename(instance_id = instanceID) %>% 
+    dplyr::select(-repeat_name, 
+                  -repeated_id,
+                  -note_hh_head_is_sub)
+  # household members (joining to people part) 
+  repeat_household_members_enumeration <- data$repeats$repeat_household_members_enumeration %>%
+    dplyr::rename(instance_id = instanceID) %>% dplyr::select(-repeat_name, -repeated_id, -hh_member_number_size) %>% filter(!is.na(permid))
+  people <- people_part %>% left_join(repeat_household_members_enumeration %>%
+                                   dplyr::select(-first_name,
+                                                 -last_name) %>%
+                                   mutate(pid = permid))
+  # mosquito nets
+  repeat_mosquito_net <- data$repeats$repeat_mosquito_net %>%
+    dplyr::rename(instance_id = instanceID,
+                  num = repeat_mosquito_net_count) %>% 
+    dplyr::select(-repeat_name, -repeated_id) %>%
+    filter(!is.na(num))
+  # water 
+  repeat_water <- data$repeats$repeat_water %>%
+    dplyr::rename(instance_id = instanceID,
+                  num = repeat_water_count) %>% 
+    dplyr::select(-repeat_name, -repeated_id) %>%
+    dplyr::filter(!is.na(num))
+  
+  # Return the formatted data
+  out <- list(df, people,
+              repeat_death_info, repeat_hh_sub, repeat_mosquito_net,
+              repeat_water)
+  names(out) <- c('main', 'people', 'repeat_death_info', 'repeat_hh_sub', 'repeat_mosquito_net',
+                  'repeat_water')
+  return(out)
+}
 
-  dbWriteTable(con, 'minicensus_main', value = insert_data, append=TRUE, row.names=FALSE)
+formatted_minicensus <- format_minicensus(data = data)
 
-  # Loop through the repeats with the same instanceID and insert them to DB
-  # Repeat Death Info
-  instance_id <- data['repeats'][[1]]['repeat_death_info'][[1]]['instanceID'][[1]][[1]]
-  # insert_data <- data.frame(instance_id = instance_id, data['repeats'][[1]]['repeat_death_info'][[1]]['all_details_except_first_2_columns'])
-
-  dbWriteTable(con, 'minicensus_repeat_death_info', value = insert_data, append=TRUE, row.names=FALSE)
-
-  # Repeat HH Sub
-  instance_id <- data['repeats'][[1]]['repeat_hh_sub'][[1]]['instanceID'][[1]][[1]]
-  # insert_data <- data.frame(instance_id = instance_id, data['repeats'][[1]]['repeat_hh_sub'][[1]]['all_details_except_first_2_columns'])
-
-  dbWriteTable(con, 'minicensus_repeat_hh_sub', value = insert_data, append=TRUE, row.names=FALSE)
-
-  # Repeat Household Members Enumeration
-  instance_id <- data['repeats'][[1]]['repeat_household_members_enumeration'][[1]]['instanceID'][[1]][[1]]
-  # insert_data <- data.frame(instance_id = instance_id, data['repeats'][[1]]['repeat_household_members_enumeration'][[1]]['all_details_except_first_2_columns'])
-
-  dbWriteTable(con, 'minicensus_repeat_household_members_enumeration', value = insert_data, append=TRUE, row.names=FALSE)
-
-  # Repeat Mosquito Net
-  instance_id <- data['repeats'][[1]]['repeat_mosquito_net'][[1]]['instanceID'][[1]][[1]]
-  # insert_data <- data.frame(instance_id = instance_id, data['repeats'][[1]]['repeat_mosquito_net'][[1]]['all_details_except_first_2_columns'])
-
-  dbWriteTable(con, 'minicensus_repeat_mosquito_net', value = insert_data, append=TRUE, row.names=FALSE)
-
-  # Repeat Water
-  instance_id <- data['repeats'][[1]]['repeat_water'][[1]]['instanceID'][[1]][[1]]
-  # insert_data <- data.frame(instance_id = instance_id, data['repeats'][[1]]['repeat_water'][[1]]['all_details_except_first_2_columns'])
-
-  dbWriteTable(con, 'minicensus_repeat_water', value = insert_data, append=TRUE, row.names=FALSE)
+for(i in 1:length(formatted_minicensus)){
+  message(i)
+  this_table <- formatted_minicensus[[i]]
+  this_name <- names(formatted_minicensus)[i]
+  dbWriteTable(con, this_name, value = this_table, append=TRUE, row.names=FALSE)
+  
 }
