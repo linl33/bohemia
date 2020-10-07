2530c05330742443c178d203a09532a89ccc10ab
Joe Brew <joebrew@gmail.com>
2020-10-07 20:55:29 +0200

    update

--- scripts/update_database.R
+++ scripts/update_database.R
@@ -132,42 +132,42 @@ if(new_data){
 }
 
 
-# # VA MOZAMBIQUE######################################################################
-# message('PULLING VA153')
-# url <- creds$databrew_odk_server
-# user = creds$databrew_odk_user
-# password = creds$databrew_odk_pass
-# id = 'va153'
-# suppressWarnings({
-#   existing_uuids <- dbGetQuery(con, 'SELECT instance_id FROM va153')
-# })
-# if (nrow(existing_uuids)< 0){
-#   existing_uuids <- c()
-# } else {
-#   existing_uuids <- existing_uuids$instance_id
-# } 
-# # Get data
-# data <- odk_get_data(
-#   url = url,
-#   id = id,
-#   id2 = id2,
-#   unknown_id2 = FALSE,
-#   uuids = NULL,
-#   exclude_uuids = existing_uuids,
-#   user = user,
-#   password = password
-# )
-# new_data <- FALSE
-# if(!is.null(data)){
-#   new_data <- TRUE
-# }
-# if(new_data){
-#   # Format data
-#   formatted_data <- format_refusals(data = data)
-#   # Update data
-#   update_refusals(formatted_data = formatted_data,
-#                   con = con)
-# }
+# VA MOZAMBIQUE######################################################################
+message('PULLING VA153')
+url <- creds$databrew_odk_server
+user = creds$databrew_odk_user
+password = creds$databrew_odk_pass
+id = 'va153'
+suppressWarnings({
+  existing_uuids <- dbGetQuery(con, 'SELECT instance_id FROM va153')
+})
+if (nrow(existing_uuids)< 0){
+  existing_uuids <- c()
+} else {
+  existing_uuids <- existing_uuids$instance_id
+}
+# Get data
+data <- odk_get_data(
+  url = url,
+  id = id,
+  id2 = id2,
+  unknown_id2 = FALSE,
+  uuids = NULL,
+  exclude_uuids = existing_uuids,
+  user = user,
+  password = password
+)
+new_data <- FALSE
+if(!is.null(data)){
+  new_data <- TRUE
+}
+if(new_data){
+  # Format data
+  formatted_data <- format_va(data = data)
+  # Update data
+  update_va(formatted_data = formatted_data,
+                  con = con)
+}
 
 
 x = dbDisconnect(con)
