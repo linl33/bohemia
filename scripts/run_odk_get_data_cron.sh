e942061c21d36ef4b4154ce5f924dae685306101
Joe Brew <joebrew@gmail.com>
2020-10-07 17:37:11 +0200

    updated

--- scripts/run_odk_get_data_cron.sh
+++ scripts/run_odk_get_data_cron.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-ROOT_FOLDER_PATH=/home/ubuntu/Documents/bohemia
+#ROOT_FOLDER_PATH=/home/ubuntu/Documents/bohemia
 # Runs odk_get_data for instance id's not already retrieved.
 echo "Started the data retrieval"
-${ROOT_FOLDER_PATH}/scripts/update_database.R
+cd ~/Documents/bohemia/scripts
+# .${ROOT_FOLDER_PATH}/scripts/update_database.R
+Rscript update_database.R
 echo "ODK data retrieval complete"
