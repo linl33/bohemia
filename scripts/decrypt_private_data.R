00bd872c4cc9a5838a79703711a5c06ad24cfa4e
Kate Kimani <katewkimani@gmail.com>
2020-11-07 13:18:02 +0100

    Update param labels to match encrypt

--- scripts/decrypt_private_data.R
+++ scripts/decrypt_private_data.R
@@ -1,13 +1,13 @@
 #' Decrypt data provided using the provided private key
 #' 
 #' @param data The private data to be decrypted
-#' @param key The filename where the key to be used is stored
+#' @param keyfile The filename where the key to be used is stored
 #' @return A plain text string
 #' @import PKI
 #' @export
 
-decrypt_private_data <- function(data, key='bohemia_priv.pem'){
-  priv.pem <- load(key, envir = parent.frame(), verbose = FALSE)
+decrypt_private_data <- function(data, keyfile='bohemia_priv.pem'){
+  priv.pem <- read.delim(keyfile)
   # load the public key
   priv.k <- PKI.load.key(priv.pem)
   # decrypt with private key
