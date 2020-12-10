93703346ffcacc74c16bd058ac71c5169ec98448
Joe Brew <joebrew@gmail.com>
2020-12-10 09:30:20 +0100

    scratch

--- scripts/run_odk_get_data_cron_scratch.sh
+++ scripts/run_odk_get_data_cron_scratch.sh
@@ -3,9 +3,6 @@
 # Runs odk_get_data for instance id's not already retrieved.
 set -e
 source /home/ubuntu/.virtualenvs/bohemia/bin/activate
-echo "Started the data retrieval"
-cd ~/Documents/bohemia/scripts
-# .${ROOT_FOLDER_PATH}/scripts/update_database.R
-Rscript update_database.R
-echo "ODK data retrieval complete"
+echo "Activated"
+echo "Deactivated"
 deactivate
