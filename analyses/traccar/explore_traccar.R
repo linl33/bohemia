9af505a350bb6d651f6195d221b9fcf4f70db1c7
benmbrew <benmbrew@gmail.com>
2020-12-13 18:52:15 -0500

    found more examples for presentation and made more plots

--- analyses/traccar/explore_traccar.R
+++ analyses/traccar/explore_traccar.R
@@ -43,14 +43,14 @@ if(!'fids.csv' %in% dir('/tmp')){
 moz_forms <- load_odk_data(the_country = 'Mozambique',
                            credentials_path = '../../credentials/credentials.yaml',
                            users_path = '../../credentials/users.yaml',
-                           local = TRUE, 
+                           local = FALSE, 
                            efficient=FALSE,
                            use_cached = FALSE)
 
 tz_forms <- load_odk_data(the_country = 'Tanzania',
                           credentials_path = '../../credentials/credentials.yaml',
                           users_path = '../../credentials/users.yaml',
-                          local = TRUE, 
+                          local = FALSE, 
                           efficient=FALSE,
                           use_cached = FALSE)
 
@@ -71,36 +71,37 @@ traccar$total_distance <- as.numeric(unlist(lapply(strsplit(traccar$total_distan
 traccar$motion <- as.character(unlist(lapply(strsplit(traccar$motion, ':'), function(x) x[2])))
 
 traccar_moz <- traccar %>% filter(unique_id %in% fids$bohemia_id[fids$country == 'Mozambique'])
-traccar_tza <- traccar %>% filter(unique_id %in% fids$bohemia_id[fids$country == 'Tanzania'])
+traccar_tz <- traccar %>% filter(unique_id %in% fids$bohemia_id[fids$country == 'Tanzania'])
 
-load('temp_data.R')
 
 # get correct time zones for census data
 moz_census$start_time <- lubridate::as_datetime(moz_census$start_time, tz = 'Africa/Maputo')
 tz_census$start_time <- lubridate::as_datetime(tz_census$start_time, tz = 'Africa/Dar_es_Salaam')
 
 # get date for census
-moz_census$date <- as.Date(moz_census$start_time, 'CAT')
-tz_census$date <- as.Date(tz_census$start_time, 'EAT')
+moz_census$date <- lubridate::date(moz_census$start_time)
+tz_census$date <- lubridate::date(tz_census$start_time)
 
 # get correct time zones for enumerations data (Moz only)
 moz_enum$start_time <- lubridate::as_datetime(moz_enum$start_time, tz = 'Africa/Maputo')
-moz_enum$date <- as.Date(moz_enum$start_time, 'CAT')
+moz_enum$date <-  lubridate::date(moz_enum$start_time)
 
 # get correct time zones for va data
 moz_va$start_time <- lubridate::as_datetime(moz_va$start_time, tz = 'Africa/Maputo')
-moz_va$date <- as.Date(moz_va$start_time, 'CAT')
+moz_va$date <- lubridate::date(moz_va$start_time)
 
 tz_va$start_time <- lubridate::as_datetime(tz_va$start_time, tz = 'Africa/Dar_es_Salaam')
-tz_va$date <- as.Date(tz_va$start_time, 'EAT')
+tz_va$date <-  lubridate::date(tz_va$start_time)
 
 # get time zones for traccar data
 traccar_moz$devicetime <- lubridate::as_datetime(traccar_moz$devicetime, tz = 'Africa/Maputo')
-traccar_moz$date <- as.Date(traccar_moz$devicetime, 'CAT')
+traccar_moz$date <-  lubridate::date(traccar_moz$devicetime)
 
 traccar_tz$devicetime <- lubridate::as_datetime(traccar_tz$devicetime, tz = 'Africa/Dar_es_Salaam')
-traccar_tz$date <- as.Date(traccar_tz$devicetime, 'EAT')
+traccar_tz$date <-  lubridate::date(traccar_tz$devicetime)
 
+# save.image('temp_data.R')
+load('temp_data.R')
 
 # take all dates out of functions
 # two function - subset_forms and subset_traccar (one country at a time)
@@ -132,12 +133,7 @@ subset_traccar <- function(temp_dat, wid_code, date_slider){
   
 }
 #### lots of junk below. put it all into one function, that takes all data (NA for enum if TZ), and layers a plot. If enumeration and/or census not available (because tz or date range selected), then dont add layer. final plot has map with data that's available.
-trac = traccar_tz
-mini = tz_census
-enum = tz_enum
-va = tz_va
-wid_code = '90'
-date_range = c("2020-11-27", "2020-11-27")
+
 plot_traccar <- function(trac, mini, enum, va, wid_code,date_range, show_line_only ){
   mini <- try(subset_forms(temp_dat = mini, wid_code = wid_code, date_slider = date_range, form = 'mini'), silent = TRUE)
   enum <- try(subset_forms(temp_dat = enum, wid_code = wid_code, date_slider = date_range, form='enum'),silent = TRUE )
@@ -218,21 +214,36 @@ get_long_travel <- function(temp_dat){
   return(temp)
 }
 
-top_travels <- get_long_travel(traccar_moz)
-top_travels <- top_travels %>% filter(date >='2020-10-02', date<='2020-12-05')
-temp <- top_travels %>% filter(unique_id=='358')
+# top_travels <- get_long_travel(traccar_moz)
+# top_travels <- top_travels %>% filter(date >='2020-10-02', date<='2020-12-05')
+# temp <- top_travels %>% filter(unique_id=='358')
 
-# 338 is good
-sort(unique(moz_census$date))
+this_wid ='358'
+temp <- moz_form_dis %>% filter(wid==this_wid)
+
+# this_wid ='89'
+# temp <- tz_form_dis %>% filter(wid==this_wid)
+# 
+# ggplot(temp, aes(date, km_to_form)) + 
+#   geom_bar(stat='identity') +
+#   labs(x='', y = 'km traveled to forms submitted') +
+#   theme_bohemia()
+#   
 
 # MOZ
-plot_traccar(trac = traccar_moz, mini = moz_census, enum = moz_enum, va = moz_va, wid_code = '358', date_range = c("2020-10-20", "2020-10-20"), show_line_only = TRUE)
+plot_traccar(trac = traccar_moz, mini = moz_census, enum = moz_enum, va = moz_va, wid_code = this_wid, date_range = c("2020-10-19", "2020-10-19"), show_line_only = FALSE)
 
 # TZ
-plot_traccar(trac = traccar_tz, mini = tz_census, enum = tz_enum, va = tz_va, wid_code = '77', date_range = c("2020-12-02", "2020-12-03"), show_line_only = TRUE)
+plot_traccar(trac = traccar_tz, mini = tz_census, enum = tz_enum, va = tz_va, wid_code = this_wid, date_range = c("2020-12-04", "2020-12-04"), show_line_only = TRUE)
 
 
-#MZ: 308, 336,338,357,358, 370, 379, 393, 394, 396, 401, 403
+
+
+
+
+
+
+#MZ:
 #TZ:44, 72, 7, 3, 85, 20, 38, 51, 77,25,27,90, 71
 
 
@@ -242,12 +253,6 @@ plot_traccar(trac = traccar_tz, mini = tz_census, enum = tz_enum, va = tz_va, wi
 #############
 # mozambique
 #############
-
-# get date object 
-moz_census$date <- as.Date(moz_census$end_time)
-moz_enum$date <- as.Date(moz_enum$end_time)
-moz_va$date <- as.Date(moz_va$end_time)
-traccar_moz$date <- as.Date(traccar_moz$devicetime)
 
 # group by wid and date 
 moz_census_group <- moz_census %>% group_by(wid, date) %>% summarise(counts=n())
@@ -260,32 +265,36 @@ moz_forms <- rbind(moz_census_group, moz_enum_group, moz_va_group)
 moz_distance <- traccar_moz %>% group_by(unique_id, date) %>% summarise(sum_travel = sum(distance, na.rm = TRUE)) %>% filter(sum_travel >0) 
 # join with moz forms 
 moz_form_dis <- inner_join(moz_forms, moz_distance, by=c('wid'='unique_id', 'date'))
-moz_form_corr <- moz_form_dis %>% group_by(wid) %>% summarise(correlation = cor(counts, sum_travel))
+moz_form_dis$km <- moz_form_dis$sum_travel/1000
+moz_form_dis$km_to_form <- moz_form_dis$km/moz_form_dis$counts
+# moz_form_corr <- moz_form_dis %>% group_by(wid) %>% summarise(correlation = cor(counts, sum_travel),
+#                                                               km_to_form = (km/counts))
+moz_form_dis <- moz_form_dis %>% filter(km_to_form <2000)
 
 # moz_form_dis <- moz_form_dis %>% filter(sum_travel <500000)
 # moz_form_dis <- moz_form_dis %>% group_by(wid) %>% summarise(mean_forms = mean(counts), 
                                                               # mean_travel = mean(sum_travel))
 options(scipen = '999')
+
+# choose individual case and plot
+ggplot(moz_form_dis ,aes(wid,km_to_form)) + geom_bar(stat='identity') +
+  labs(x = 'Individual fieldworker days',
+       y='km traveled to form ratio') +
+  theme_bohemia() +
+  theme(axis.text.x = element_blank(),
+        axis.ticks.x = element_blank())
+
 # plot corelation
 ggplot(moz_form_dis %>% filter(wid=='316'), aes(counts, sum_travel)) + geom_point() +
   geom_smooth(method = 'lm', se = FALSE, lty=2) +
   labs(x='Forms submitted', 
        y='Distance Traveled (meters)')
 
-# choose individual case and plot
-ggplot(moz_form_corr %>% filter(correlation >-.9, correlation<1), aes(wid,correlation)) + geom_bar(stat='identity') +
-  labs(x = 'Worker ID',
-       y='Correlation') +
-  theme_bohemia()
+
 
 #############
 # tanzania
 #############
-# get date object 
-tz_census$date <- as.Date(tz_census$end_time)
-tz_va$date <- as.Date(tz_va$end_time)
-traccar_tz$date <- as.Date(traccar_tz$devicetime)
-
 # group by wid and date 
 tz_census_group <- tz_census %>% group_by(wid, date) %>% summarise(counts=n())
 tz_va_group <- tz_va %>% group_by(wid, date) %>% summarise(counts=n())
@@ -296,12 +305,23 @@ tz_forms <- rbind(tz_census_group,  tz_va_group)
 tz_distance <- traccar_tz %>% group_by(unique_id, date) %>% summarise(sum_travel = sum(distance, na.rm = TRUE)) %>% filter(sum_travel >0) 
 # join with tz forms 
 tz_form_dis <- inner_join(tz_forms, tz_distance, by=c('wid'='unique_id', 'date'))
-tz_form_corr <- tz_form_dis %>% group_by(wid) %>% summarise(correlation = cor(counts, sum_travel))
+# tz_form_corr <- tz_form_dis %>% group_by(wid) %>% summarise(correlation = cor(counts, sum_travel))
+tz_form_dis$km <- tz_form_dis$sum_travel/1000
+tz_form_dis$km_to_form <- tz_form_dis$km/tz_form_dis$counts
 
 # tz_form_dis <- tz_form_dis %>% filter(sum_travel <500000)
 # tz_form_dis <- tz_form_dis %>% group_by(wid) %>% summarise(mean_forms = mean(counts), 
 # mean_travel = mean(sum_travel))
 options(scipen = '999')
+
+
+# choose individual case and plot
+ggplot(tz_form_dis ,aes(wid,km_to_form)) + geom_bar(stat='identity') +
+  labs(x = 'Individual fieldworker days',
+       y='km traveled to form ratio') +
+  theme_bohemia() +
+  theme(axis.text.x = element_blank(),
+        axis.ticks.x = element_blank())
 
 # TZ:44, 72, 7, 3, 85, 20, 38, 51, 77,25,27,90
 # plot corelation
@@ -309,12 +329,6 @@ ggplot(tz_form_dis %>% filter(wid=='90'), aes(counts, sum_travel)) + geom_point(
   geom_smooth(method = 'lm', se = FALSE, lty=2) +
   labs(x='Forms submitted', 
        y='Distance Traveled (meters)')
-
-# choose individual case and plot
-ggplot(tz_form_corr %>% filter(correlation >-.9, correlation<1), aes(wid,correlation)) + geom_bar(stat='identity') +
-  labs(x = 'Worker ID',
-       y='Correlation') +
-  theme_bohemia()
 
 ################
 # code for intro to slides 
@@ -367,3 +381,13 @@ trac_tz_dec <- traccar_tz %>% filter(date>'2020-11-30') %>% group_by(unique_id) 
 length(which(!moz_fw$wid %in% trac_moz_dec$unique_id))
 length(which(!tz_fw$wid %in% trac_tz_dec$unique_id))
 
+# make a stacked bar plot
+temp <- tibble(`Active FWs in December`= c(84, 74),
+       `Using traccar`= c(19, 27),
+       `Country`= c('Mozambique', 'Tanzania'))
+library(reshape2)
+temp <- melt(temp, id.vars = 'Country')
+ggplot(temp, aes(Country, value, fill= variable)) + geom_bar(stat='identity', alpha = 0.6) +
+  scale_fill_manual(name = '', values = c('darkred', 'darkgreen')) +
+  labs(y='# of FWs') +
+  theme_bohemia()
