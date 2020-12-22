51851d75328297d49492f2c2da7b6ec426278c1c
benmbrew <benmbrew@gmail.com>
2020-12-22 14:29:03 -0500

    added code for xing to get quick query statements from error ids

--- scripts/get_database_fixes.R
+++ scripts/get_database_fixes.R
@@ -26,7 +26,7 @@ fixes <- odk_data$fixes
 fixes_ids <- fixes$id
 
 # keep only response_details, id, and instance_id
-corrections <- corrections %>% filter(!id %in% fixes_ids) %>% select(id, instance_id, response_details)
+# corrections <- corrections %>% filter(!id %in% fixes_ids) %>% select(id, instance_id, response_details)
 write.csv(corrections, '~/Desktop/temporary_corrections.csv')
 
 minicensus_main <- odk_data$minicensus_main
@@ -44,6 +44,41 @@ subs <- odk_data$minicensus_repeat_hh_sub
 
 others <- c()
 
+# error_id <- 'repeat_hh_id_0d09707a-51be-4e91-a5e8-5534fb7bd007,7f97b88a-4090-4c81-aab7-b26fa51d5e99,fe970bb0-521c-4a82-b7d7-b8ef282b1bbb'
+
+# get_num <- function(error_id){
+#   inst_id <- unlist(lapply(strsplit(error_id, split = ','), function(x) x[length(x)]))
+#   num_people <- minicensus_main$hh_member_num[minicensus_main$instance_id == inst_id]
+#   return(num_people)
+# }
+# 
+error_id <- 'repeat_hh_id_358d32a0-480d-4a2a-b507-5244f92a2ecf,8b133ccc-2f0d-439e-ab6d-06bb7b3d16eb'
+# create function to get number of people in household 
+get_query <- function(error_id){
+  inst_id <- unlist(lapply(strsplit(error_id, split = ','), function(x) x[length(x)]))
+  temp <- corrections %>% filter(id == error_id)
+  # get new hh id
+  new_hh_id <- trimws(unlist(lapply(strsplit(temp$response_details, split = 'to'), function(x) x[length(x)])), which = 'both')
+  # UPDATE clean_minicensus_main SET hh_id='DEU-216' WHERE instance_id='8b133ccc-2f0d-439e-ab6d-06bb7b3d16eb'
+  # get household id query (first part)
+  hh_query <- paste0("UPDATE clean_minicensus_main SET hh_id='", new_hh_id, "' WHERE instance_id='", inst_id, "'")
+  
+  # get people query (second part)
+  old_hh_id <- minicensus_main$hh_id[minicensus_main$instance_id == inst_id]
+  people$hh_id <- substr(people$pid, 1, 7)
+  temp <- people %>% filter(hh_id == old_hh_id) %>% select(pid, num)
+  result_list <- list()
+  for(i in 1:nrow(temp)){
+    result_list[[i]] <- paste0("UPDATE clean_minicensus_people SET pid = '", temp$pid[i],"'", " WHERE num='", temp$num[i],"'", " and instance id='", inst_id,"'" )
+  }
+  people_query=  paste0(unlist(result_list), collapse = ';')
+  
+  # combine queries separated by ;
+  full_query = paste0(hh_query, ';', people_query )
+  return(full_query)
+}
+
+get_query(error_id= 'repeat_hh_id_358d32a0-480d-4a2a-b507-5244f92a2ecf,8b133ccc-2f0d-439e-ab6d-06bb7b3d16eb')
 
 # eldo <- readr::read_csv('~/Desktop/eldo.csv')
 # ids <- eldo$`meta:instanceID`
