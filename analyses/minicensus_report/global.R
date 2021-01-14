806c2289613c643f6c4509e9f77aff462ae7e661
Joe Brew <joebrew@gmail.com>
2021-01-14 13:30:41 +0100

    minicensus report

--- analyses/minicensus_report/global.R
+++ analyses/minicensus_report/global.R
@@ -15,27 +15,30 @@ library(htmlTable)
 library(readxl)
 
 # Read in xls form
-xf <- read_excel('../../forms/minicensus/minicensus.xlsx') %>% dplyr::select(name,
+xf <- read_excel('../../forms/smallcensusb/smallcensusb.xlsx') %>% dplyr::select(name,
                                                       en = `label::English`,
                                                       sw = `label::Swahili`)
+
+# Read in imani results
+imani <- read_csv('imani.csv')
 
 # read in creds
 creds <- read_yaml('../../credentials/credentials.yaml')
 
+
+
 # Read in answer key
 if(!'data.RData' %in% dir()){
+  
+  uuids <- imani$`meta:instanceID`
   data <- odk_get_data(
-    url = creds$tza_odk_server,
-    id = 'minicensus',
+    url = creds$databrew_odk_server,
+    id = 'smallcensusb',
     id2 = NULL,
     unknown_id2 = FALSE,
-    # uuids = NULL,
-    exclude_uuids = c("uuid:06e9b414-4c82-4097-bd4a-e31065f1265e",
-                      "uuid:641eaadd-1e47-4e48-99c2-02c13de93b0a",
-                      "uuid:bab37889-d8a8-4b6c-984c-a99e2b9926ce",
-                      "uuid:ab79756d-ae1a-44d2-a9f8-424a6c88391f"), # this one indicated as incorrect by email
-    user = creds$tza_odk_user,
-    password = creds$tza_odk_pass
+    uuids = uuids,
+    user = creds$databrew_odk_user,
+    password = creds$databrew_odk_pass
   )
   save(data, file = 'data.RData')
 } else {
