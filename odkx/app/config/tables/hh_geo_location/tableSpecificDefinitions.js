bfafa8416739fe07aecc4594ca54eb6c960565f7
linl33 <linlee1997@gmail.com>
2021-02-04 22:25:07 +0800

    Revert "Add translations to forms"
    This reverts commit d21aa63e051894407e7d2f4a3332b6900bd1b9a1.

--- odkx/app/config/tables/hh_geo_location/tableSpecificDefinitions.js
+++ odkx/app/config/tables/hh_geo_location/tableSpecificDefinitions.js
@@ -1,14 +1,3 @@
 window.odkTableSpecificDefinitions = {
-  "_tokens": {
-    "stand_outside": {
-      "string_token": "stand_outside",
-      "text": "Stand just outside the front door of the house and geocode the location.",
-      "_row_num": 2
-    },
-    "stand_outside.hint": {
-      "string_token": "stand_outside.hint",
-      "text": "The household id is {{data.hh_id}}",
-      "_row_num": 3
-    }
-  }
+  "_tokens": {}
 }
