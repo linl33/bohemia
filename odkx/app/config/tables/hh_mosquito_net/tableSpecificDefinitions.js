d21aa63e051894407e7d2f4a3332b6900bd1b9a1
linl33 <linlee1997@gmail.com>
2021-02-04 19:35:39 +0800

    Add translations to forms

--- odkx/app/config/tables/hh_mosquito_net/tableSpecificDefinitions.js
+++ odkx/app/config/tables/hh_mosquito_net/tableSpecificDefinitions.js
@@ -1,3 +1,139 @@
 window.odkTableSpecificDefinitions = {
-  "_tokens": {}
+  "_tokens": {
+    "q55a": {
+      "string_token": "q55a",
+      "text": {
+        "default": "55a. How long ago did you obtain the net?",
+        "pt": "55a. Há quanto tempo adquiriu a rede?",
+        "sw": "55a. Ulipata chandarua hiki muda gani uliopita?"
+      },
+      "_row_num": 2
+    },
+    "q55b": {
+      "string_token": "q55b",
+      "text": {
+        "default": "55b. Did someone sleep inside this net last night?",
+        "pt": "55b. Alguém dormiu dentro desta rede na noite passada?",
+        "sw": "55b. Je, kunaye aliyelala ndani ya chandarua hiki usiku uliopita?"
+      },
+      "_row_num": 3
+    },
+    "q55c": {
+      "string_token": "q55c",
+      "text": {
+        "default": "55c. Is the net being used for anything else other than sleeping under?",
+        "pt": "55c. A rede está a ser usada para outra finalidade que não seja dormir debaixo da mesma?",
+        "sw": "55c. Je, chandarua hiki kinatumika kwa kazi nyingine yoyote isipokuwa kulala ndani yake?"
+      },
+      "_row_num": 4
+    },
+    "yes": {
+      "string_token": "yes",
+      "text": {
+        "default": "Yes",
+        "pt": "Sim",
+        "sw": "Ndiyo"
+      },
+      "_row_num": 5
+    },
+    "no": {
+      "string_token": "no",
+      "text": {
+        "default": "No",
+        "pt": "Não",
+        "sw": "Hapana"
+      },
+      "_row_num": 6
+    },
+    "dk": {
+      "string_token": "dk",
+      "text": {
+        "default": "Don't Know",
+        "pt": "Não sabe",
+        "sw": "Sijui"
+      },
+      "_row_num": 7
+    },
+    "1_year_ago_or_less": {
+      "string_token": "1_year_ago_or_less",
+      "text": {
+        "default": "1 year ago or less",
+        "pt": "Há 1 ano ou menos",
+        "sw": "Mwaka 1 uliopita au chini"
+      },
+      "_row_num": 8
+    },
+    "within_the_past_1-3_years": {
+      "string_token": "within_the_past_1-3_years",
+      "text": {
+        "default": "Within the past 1-3 years",
+        "pt": "Nos últimos 1-3 anos",
+        "sw": "Ndani ya mwaka 1-3 iliyopita"
+      },
+      "_row_num": 9
+    },
+    "more_than_3_years": {
+      "string_token": "more_than_3_years",
+      "text": {
+        "default": "More than 3 years",
+        "pt": "Há mais de 3 anos",
+        "sw": "Zaidi ya miaka 3 iliyopita"
+      },
+      "_row_num": 10
+    },
+    "sleep_only": {
+      "string_token": "sleep_only",
+      "text": {
+        "default": "No, just for sleeping under",
+        "pt": "Não, apenas para dormir debaixo dela",
+        "sw": "Hapana, kulala ndani yake tu"
+      },
+      "_row_num": 11
+    },
+    "garden_netting": {
+      "string_token": "garden_netting",
+      "text": {
+        "default": "For garden netting",
+        "pt": "Para rede de jardim",
+        "sw": "Inatumika kama uzio wa bustani"
+      },
+      "_row_num": 12
+    },
+    "fishing": {
+      "string_token": "fishing",
+      "text": {
+        "default": "For fishing",
+        "pt": "Para pescar",
+        "sw": "Kutega samaki"
+      },
+      "_row_num": 13
+    },
+    "surrounding_hennery/hennery_construction": {
+      "string_token": "surrounding_hennery/hennery_construction",
+      "text": {
+        "default": "To surround hennery/hennery construction",
+        "pt": "Para cercar galinhas/capoeiras/",
+        "sw": "Uzio/ujenzi wa banda la kuku"
+      },
+      "_row_num": 14
+    },
+    "preparing_cereals/corn": {
+      "string_token": "preparing_cereals/corn",
+      "text": {
+        "default": "To prepare cereals/corn",
+        "pt": "Para preparar cereais/milho",
+        "sw": "Kuandaa nafaka/mahindi, mchele ngano na mtama"
+      },
+      "_row_num": 15
+    },
+    "cover_vegetables_crops": {
+      "string_token": "cover_vegetables_crops",
+      "text": {
+        "default": "To protect/cover vegetables/crops",
+        "pt": "Para proteger/cobrir hortas/culturas",
+        "sw": "Kulinda/kufunika mboga/mimea"
+      },
+      "_row_num": 16
+    }
+  }
 }
--- odkx/app/config/tables/hh_water_body/tableSpecificDefinitions.js
+++ odkx/app/config/tables/hh_water_body/tableSpecificDefinitions.js
@@ -1,3 +1,59 @@
 window.odkTableSpecificDefinitions = {
-  "_tokens": {}
+  "_tokens": {
+    "53ai": {
+      "string_token": "53ai",
+      "text": {
+        "default": "53a(i). Record GPS coordinates for this body of water (mandatory)",
+        "pt": "53a(i). Registar as coordenadas GPS dessa corpo de água (obrigatório)",
+        "sw": "53a (i). Chukuwa alama za GPS ya eneo hili la maji (lazima)"
+      },
+      "_row_num": 2
+    },
+    "53aii": {
+      "string_token": "53aii",
+      "text": {
+        "default": "53a(ii). What is the type of water body close (<50m) to the household?",
+        "pt": "53a(ii). Qual é o tipo de corpo de água perto (<50 m) do agregado familiar?",
+        "sw": "53a (ii). Eneo la maji lililo karibu (< 50m) na kaya yako ni la aina gani?"
+      },
+      "_row_num": 3
+    },
+    "hh_id_note": {
+      "string_token": "hh_id_note",
+      "text": "Household ID: {{data.hh_id}}",
+      "_row_num": 4
+    },
+    "water_detail": {
+      "string_token": "water_detail",
+      "text": "Water Body Details",
+      "_row_num": 5
+    },
+    "permanent": {
+      "string_token": "permanent",
+      "text": {
+        "default": "Permanent water (all year)",
+        "pt": "Água permanente (todo o ano)",
+        "sw": "Maji ya kudumu (mwaka mzima)"
+      },
+      "_row_num": 6
+    },
+    "semi_permanent": {
+      "string_token": "semi_permanent",
+      "text": {
+        "default": "Semi-permanent water (most of the year)",
+        "pt": "Água semipermanente (a maior parte do ano)",
+        "sw": "Maji ya kudumu kwa muda (wakati mwingi wa mwaka)"
+      },
+      "_row_num": 7
+    },
+    "transient": {
+      "string_token": "transient",
+      "text": {
+        "default": "Transient water (only present when it rains)",
+        "pt": "Água transitória (presente apenas quando chove)",
+        "sw": "Maji ya muda mfupi (huwepo kunaponyesha tu)"
+      },
+      "_row_num": 8
+    }
+  }
 }
