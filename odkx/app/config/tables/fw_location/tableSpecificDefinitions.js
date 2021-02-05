4a88c8fb0b65f93d607f46dbca131f34ed03e78f
linl33 <linlee1997@gmail.com>
2021-02-05 19:37:14 +0800

    Bring back translation

--- odkx/app/config/tables/fw_location/tableSpecificDefinitions.js
+++ odkx/app/config/tables/fw_location/tableSpecificDefinitions.js
@@ -1,3 +1,14 @@
 window.odkTableSpecificDefinitions = {
-  "_tokens": {}
+  "_tokens": {
+    "stand_outside": {
+      "string_token": "stand_outside",
+      "text": "Stand just outside the front door of the house and geocode the location.",
+      "_row_num": 2
+    },
+    "stand_outside.hint": {
+      "string_token": "stand_outside.hint",
+      "text": "The household id is {{data.hh_id}}",
+      "_row_num": 3
+    }
+  }
 }
